237ecfd477ef6574209b7f9a591608b7f4d11331
Błażej Kasperski <kasperski.blazej@gmail.com>
2019-10-11 10:24:46 +0200

    prepared fetch for data

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react'
 import './App.css'
+import Weather from './weather.json'
 
 class App extends Component {
 
   state = {
+    data: {}
   }
 
-  render() {     
+  componentDidMount() {
+    const url = 'https://api.openweathermap.org/data/2.5/weather?q=Warsaw&units=metric&appid=44436d16bd424daef5fce773bf6fe022'
+    fetch(url)
+    .then(data => this.setState ({ data }))
+    console.log(Weather)
+  }
 
-    return (
+  render() {
+        return (
       <div className="container">
+        
       </div>      
     )
   }
 }
 
 export default App
+
+// 790 838 616
