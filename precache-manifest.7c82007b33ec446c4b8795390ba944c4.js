4813f359f57e78c60fcbf2de8a0c54d93742fca4
Ampex <kasperski.blazej@gmail.com>
2020-01-03 17:14:26 +0100

    Updates

---
+++ precache-manifest.7c82007b33ec446c4b8795390ba944c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51988fa514a42ed1d6505394f8964816",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "28703a32b9e6b83c3aaf",
+    "url": "/weather/static/css/main.c94ae894.chunk.css"
+  },
+  {
+    "revision": "89d29263cf33b6e4a8b3",
+    "url": "/weather/static/js/2.459e15d0.chunk.js"
+  },
+  {
+    "revision": "01eb55334b76163e8ee06c07edb5382c",
+    "url": "/weather/static/js/2.459e15d0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "28703a32b9e6b83c3aaf",
+    "url": "/weather/static/js/main.2dd382fd.chunk.js"
+  },
+  {
+    "revision": "6d64a6a58c9fd6bb79cc",
+    "url": "/weather/static/js/runtime-main.9537b3a6.js"
+  }
+]);
